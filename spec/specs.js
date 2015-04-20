9729f552881dc78d5ef1a0621b56665e5ad1e19e
kyle <icegoat01@gmail.com>
2015-04-19 23:16:50 -0700

    Change properties to capital case

--- spec/specs.js
+++ spec/specs.js
@@ -2,75 +2,75 @@ describe('Pizza', function() {
 
   describe('getPrice()', function() {
 
-    it("will return a price of 10 for a small cheese pizza", function() {
+    it("will return a price of 10 for a Small Cheese pizza", function() {
       var test_pizza = Object.create(Pizza);
       test_pizza.init();
-      test_pizza.size = "small";
-      test_pizza.topping = "cheese";
+      test_pizza.size = "Small";
+      test_pizza.topping = "Cheese";
       expect(test_pizza.getPrice()).to.equal(10);
     });
 
-    it("will return a price of 13 for a medium cheese pizza", function() {
+    it("will return a price of 13 for a Medium Cheese pizza", function() {
       var test_pizza = Object.create(Pizza);
       test_pizza.init();
-      test_pizza.size = "medium";
-      test_pizza.topping = "cheese";
+      test_pizza.size = "Medium";
+      test_pizza.topping = "Cheese";
       expect(test_pizza.getPrice()).to.equal(13);
     });
 
-    it("will return a price of 17 for a large cheese pizza", function() {
+    it("will return a price of 17 for a Large Cheese pizza", function() {
       var test_pizza = Object.create(Pizza);
       test_pizza.init();
-      test_pizza.size = "large";
-      test_pizza.topping = "cheese";
+      test_pizza.size = "Large";
+      test_pizza.topping = "Cheese";
       expect(test_pizza.getPrice()).to.equal(17);
     });
 
-    it("will return a price of 12 for a small veggie pizza", function() {
+    it("will return a price of 12 for a Small Vegetarian pizza", function() {
       var test_pizza = Object.create(Pizza);
       test_pizza.init();
-      test_pizza.size = "small";
-      test_pizza.topping = "veggie";
+      test_pizza.size = "Small";
+      test_pizza.topping = "Vegetarian";
       expect(test_pizza.getPrice()).to.equal(12);
     });
 
-    it("will return a price of 15 for a medium veggie pizza", function() {
+    it("will return a price of 15 for a Medium Vegetarian pizza", function() {
       var test_pizza = Object.create(Pizza);
       test_pizza.init();
-      test_pizza.size = "medium";
-      test_pizza.topping = "veggie";
+      test_pizza.size = "Medium";
+      test_pizza.topping = "Vegetarian";
       expect(test_pizza.getPrice()).to.equal(15);
     });
 
-    it("will return a price of 19 for a large veggie pizza", function() {
+    it("will return a price of 19 for a Large Vegetarian pizza", function() {
       var test_pizza = Object.create(Pizza);
       test_pizza.init();
-      test_pizza.size = "large";
-      test_pizza.topping = "veggie";
+      test_pizza.size = "Large";
+      test_pizza.topping = "Vegetarian";
       expect(test_pizza.getPrice()).to.equal(19);
     });
 
-    it("will return a price of 14 for a small pepperoni pizza", function() {
+    it("will return a price of 14 for a Small Pepperoni pizza", function() {
       var test_pizza = Object.create(Pizza);
       test_pizza.init();
-      test_pizza.size = "small";
-      test_pizza.topping = "pepperoni";
+      test_pizza.size = "Small";
+      test_pizza.topping = "Pepperoni";
       expect(test_pizza.getPrice()).to.equal(14);
     });
 
-    it("will return a price of 17 for a medium pepperoni pizza", function() {
+    it("will return a price of 17 for a Medium Pepperoni pizza", function() {
       var test_pizza = Object.create(Pizza);
       test_pizza.init();
-      test_pizza.size = "medium";
-      test_pizza.topping = "pepperoni";
+      test_pizza.size = "Medium";
+      test_pizza.topping = "Pepperoni";
       expect(test_pizza.getPrice()).to.equal(17);
     });
 
-    it("will return a price of 21 for a large pepperoni pizza", function() {
+    it("will return a price of 21 for a Large Pepperoni pizza", function() {
       var test_pizza = Object.create(Pizza);
       test_pizza.init();
-      test_pizza.size = "large";
-      test_pizza.topping = "pepperoni";
+      test_pizza.size = "Large";
+      test_pizza.topping = "Pepperoni";
       expect(test_pizza.getPrice()).to.equal(21);
     });
 
